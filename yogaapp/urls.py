8a9f4b6434e12c73c7fc2f092a1973fb520e5885
vyshnav <vyshnavofc@gmail.com>
2024-01-13 18:44:00 +0530

    first

---
+++ yogaapp/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views
+app_name="yogaapp"
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('200HoursYogaTeachersTrainingCourse', views.twohundred, name='200HoursYogaTeachersTrainingCourse'),
+    path('300HoursYogaTeachersTrainingCourse', views.threehundred, name='300HoursYogaTeachersTrainingCourse'),
+    path('YogaPhilosophyTeachersTraining', views.philosophy, name='YogaPhilosophyTeachersTraining'),
+    path('ChakraHealerCourse', views.chakra, name='ChakraHealerCourse'),
+    path('AncientCosmicEnergyHealingProcess', views.cosmic, name='AncientCosmicEnergyHealingProcess'),
+    path('PrenatalandPostnatalYogaTTC', views.natal, name='PrenatalandPostnatalYogaTTC'),
+    path('whatsapp', views.whatsapp, name='whatsapp'),
+    path('test', views.test, name='test'),
+    path('instructor', views.instructor, name='instructor'),
+    path('Become_A_Healer', views.Become_A_Healer, name='Become_A_Healer'),
+    path('Spiritual_Retreat', views.Spiritual_Retreat, name='Spiritual_Retreat'),
+    path('Contact', views.Contact, name='Contact'),
+]
+
---
+++ yogaapp/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render
+from django.shortcuts import redirect
+
+# Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+def twohundred(request):
+    return render(request, "200hrs.html")
+
+def threehundred(request):
+    return render(request, "300hrs.html")
+
+def philosophy(request):
+    return render(request, "philosophy.html")
+
+def chakra(request):
+    return render(request, "chakra.html")
+
+def cosmic(request):
+    return render(request, "cosmic.html")
+
+def natal(request):
+    return render(request, "natal.html")
+
+def Contact(request):
+    return render(request, "contact.html")
+
+def test(request):
+    return render(request, "test.html")
+
+def instructor(request):
+    return render(request, "instructor.html")
+
+def whatsapp(request):
+    whatsapp_number = '919008663139'
+
+    # Generate the WhatsApp URL
+    whatsapp_url = f'https://api.whatsapp.com/send?phone={whatsapp_number}'
+
+    # Redirect the user to the WhatsApp URL
+    return redirect(whatsapp_url)
+
+
+def Become_A_Healer(request):
+    return render(request, "image.html")
+
+
+def Spiritual_Retreat(request):
+    return render(request, "spirit.html")
