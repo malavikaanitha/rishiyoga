fa49180aa530565d2ee06ce0b62fae349522b1ac
vyshnav <vyshnavofc@gmail.com>
2024-01-14 12:23:11 +0530

    vercel

--- yogattc/settings.py
+++ yogattc/settings.py
@@ -74,12 +74,7 @@ WSGI_APPLICATION = 'yogattc.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#   
 
 
 # Password validation
@@ -117,7 +112,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS =os.path.join(BASE_DIR,'static'),
+STATIC_ROOT =os.path.join(BASE_DIR,'staticfiles_build','static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
